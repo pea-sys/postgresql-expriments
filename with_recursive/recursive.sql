26faac0f573a44fdca543c9ba360a2c09e8efad7
pea-sys <pea98258@gmail.com>
2022-01-01 17:25:08 +0900

    with recursive

---
+++ with_recursive/recursive.sql
@@ -0,0 +1,6 @@
+WITH RECURSIVE rec(organization_id ,name, parent_id) AS (
+    select * from organization_tbl where organization_id = 1
+    union all
+    select b.organization_id,b.name,b.parent_id from rec a join organization_tbl b
+    on a.organization_id = b.parent_id)
+select organization_id, name, parent_id from rec;
